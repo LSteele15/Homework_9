3aa4e63c506cf01af11938e20f6189aa9bcf381f
LSteele15 <89127608+LSteele15@users.noreply.github.com>
2021-10-01 00:43:33 +1000

    Add files via upload

---
+++ greater_melb_income.js
@@ -0,0 +1,4 @@
+var vg_1 = "Greater_Melb_Income.vega";
+vegaEmbed("#greater_melb", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
